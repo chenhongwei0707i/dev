dcb86f498fd9e6437ea7d2ddd45d25268bc27a98
makejava <63669689@qq.com>
2021-10-14 15:05:51 +0800

    开启版本控制

---
+++ src/main/java/com/chw/project_pro_max/ProjectProMaxApplication.java
@@ -0,0 +1,13 @@
+package com.chw.project_pro_max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProMaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectProMaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chw/project_pro_max/ProjectProMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chw.project_pro_max;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProMaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
